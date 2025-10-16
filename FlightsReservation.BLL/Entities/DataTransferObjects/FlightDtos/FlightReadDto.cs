7079d7bc5a78a2908c81c796b3291c37db339b9b
Heorhii Mikula <gera8337@gmail.com>
2025-10-16 21:20:33 +0200

    Seats are now available with FlightReadDto.

--- FlightsReservation.BLL/Entities/DataTransferObjects/FlightDtos/FlightReadDto.cs
+++ FlightsReservation.BLL/Entities/DataTransferObjects/FlightDtos/FlightReadDto.cs
@@ -1,7 +1,10 @@
-﻿namespace FlightsReservation.BLL.Entities.DataTransferObjects.FlightDtos;
+﻿using FlightsReservation.BLL.Entities.DataTransferObjects.SeatDtos;
+
+namespace FlightsReservation.BLL.Entities.DataTransferObjects.FlightDtos;
 
 public class FlightReadDto : BaseFlightDto
 {
     public Guid Id { get; set; }
     public int AvailableSeats { get; set; }
+    public List<SeatReadDto> Seats { get; set; } = null!;
 }
